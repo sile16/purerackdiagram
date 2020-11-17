5c079d88bb0dc27eddd5e11a592c912a252e28fe
Matt Robertson <sile16@gmail.com>
2020-11-17 12:17:25 -0600

    add new c sizes

--- ui/CONFIG.js
+++ ui/CONFIG.js
@@ -5,13 +5,13 @@ API_ENDPOINT = "https://61fuj0h54e.execute-api.us-east-1.amazonaws.com/default/r
 // the options values for FlashArray configurations
 //
 var FA_OPTIONS = {
-    model: ['fa-x10r3', 'fa-x20r3', 'fa-x50r3', 'fa-x70r3', 'fa-x90r3', 
+    model: ['fa-x10r3', 'fa-x20r3', 'fa-x50r3', 'fa-x70r3', 'fa-x90r3',
             'fa-x10r2', 'fa-x20r2', 'fa-x50r2', 'fa-x70r2', 'fa-x90r2',
             'fa-x70r1', 'fa-c60', 'fa-m10r2', 'fa-m20r2', 'fa-m50r2', 'fa-m70r2'],
     protocol: ['fc', 'eth'],
     face: ['front', 'back'],
     datapacks: '3/127-45/0',
-    csizes: ['345', '366', '494', '839', '879', '1185', '1390'],
+    csizes: ['247', '345', '366', '494', '839', '879', '984', '1185', '1390', '1531', '1672', '1877'],
     bezel: ['FALSE', 'TRUE'],
     direction: ['up', 'down'],
     fm_label: ['TRUE', 'FALSE'],
