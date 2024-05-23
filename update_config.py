feeed9ef40f9fa7dabd7510b237e6072331e29f6
Matt Robertson <sile16@gmail.com>
2024-05-23 18:56:59 -0500

    add in dca card to xl130 & xl170

--- update_config.py
+++ update_config.py
@@ -170,10 +170,10 @@ def static_global_config():
             "fa-c40r3-fc": ["2fc", None, None, None],
             "fa-c60r3-eth": ["2eth", None, None, None],
             "fa-c60r3-fc": ["4fc", None, None, None],
-            "fa-xl130r1-eth": [None, "2eth", None, None, None, None, None, None, None],
-            "fa-xl130r1-fc": [None, "4fc", None, None, None, None, None, None, "2fc"],
-            "fa-xl170r1-eth": [None, "2eth", None, None, None, None, None, None, None],
-            "fa-xl170r1-fc": [None, "4fc", None, None, None, None, None, None, "2fc"]
+            "fa-xl130r1-eth": [None, "2eth", None, None, None, "dca", None, None, None],
+            "fa-xl130r1-fc": [None, "4fc", None, None, None, "dca", None, None, "2fc"],
+            "fa-xl170r1-eth": [None, "2eth", None, None, None, "dca", None, None, None],
+            "fa-xl170r1-fc": [None, "4fc", None, None, None, "dca", None, None, "2fc"]
 
         },
 
